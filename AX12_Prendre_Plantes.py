28fc0c1987d9bf317de17177bb852626528a7379
Makalé Touré <makale.toure03@gmail.com>
2024-04-05 18:22:35 +0200

    New_version

--- AX12_Prendre_Plantes.py
+++ AX12_Prendre_Plantes.py
@@ -2,45 +2,39 @@ import time
 from AX12_Pinces import AX12_Pinces
 from AX12_Ascenseur import AX12_Ascenseur
 
-class AX12_Prendre_Plantes:
-    def __init__(self):
-        # Initialisation des instances des classes AX12_Pinces et AX12_Ascenseur
-        self.ax12_pinces = AX12_Pinces()
-        self.ax12_ascenseur = AX12_Ascenseur()
+def prendre_plante():
+    # Initialisation des instances des classes AX12_Pinces et AX12_Ascenseur
+    ax12_pinces = AX12_Pinces()
+    ax12_ascenseur = AX12_Ascenseur()
+    
+    # Initialiser les moteurs des pinces et de l'ascenseur
+    ax12_pinces.initialize_motors()
+    ax12_ascenseur.initialize_motors()
 
-    def prendre_plantes(self):
-        # Ouvrir la pince pour prendre les plantes
-        self.ax12_pinces.open_pince()
-        
-        # Fermer la pince pour prendre les plantes
-        self.ax12_pinces.close_pince()
+    # Ouvrir la pince pour prendre les plantes
+    ax12_pinces.open_pince()
+    
+    # Fermer la pince pour prendre les plantes
+    ax12_pinces.close_pince()
 
-        # Monter l'ascenseur
-        self.ax12_ascenseur.elevate()
-        
-        time.sleep(10)
+    # Monter l'ascenseur
+    ax12_ascenseur.elevate()
+    
+    time.sleep(10)
 
-        # Descendre l'ascenseur
-        self.ax12_ascenseur.lower_for_plant()
+    # Descendre l'ascenseur
+    ax12_ascenseur.lower_for_plant()
 
-        # Ouvrir à nouveau la pince pour déposer les plantes
-        self.ax12_pinces.open_pince_stepbystep()
-        
-        # Monter l'ascenseur
-        self.ax12_ascenseur.elevate()
-        
-        # Fermer la pince à nouveau
-        self.ax12_pinces.close_pince()
+    # Ouvrir à nouveau la pince pour déposer les plantes
+    ax12_pinces.open_pince_stepbystep()
+    
+    # Monter l'ascenseur
+    ax12_ascenseur.elevate()
+    
+    # Fermer la pince à nouveau
+    ax12_pinces.close_pince()
 
-    def run(self):
-        # Initialiser les moteurs des pinces et de l'ascenseur
-        self.ax12_pinces.initialize_motors()
-        self.ax12_ascenseur.initialize_motors()
-
-        # Prendre les plantes
-        self.prendre_plantes()
-
-# Exemple d'utilisation
 if __name__ == "__main__":
-    prendre_plantes = AX12_Prendre_Plantes()
-    prendre_plantes.run()
+    # Prendre les plantes
+    prendre_plante()
+
